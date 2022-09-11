986eb276d54ae1941bfb65c83473e4d88b40e3b1
Ijeoma-ukwe <111155128+Ijeoma-ukwe@users.noreply.github.com>
2022-09-11 17:48:21 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - prints all possible combinations of single-digit numbers,
+* main - prints all possible combinations of single-digit numbers
+* followed by a new line
 * Return: Always 0 (Success)
 */
 int main(void)
