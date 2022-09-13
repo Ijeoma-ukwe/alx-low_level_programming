410c170a260828ed9563dd9306f2fd88c1f6f39f
https://github.com/Ijeoma-ukwe/alx-low_level_programming.git <ukwejennifer@gmail.com>
2022-09-13 14:13:09 -0700

    a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
-1-main.c
+#include "main.h"
+
+/**
+* print-alphabet - function that prints the alphabet in lowercase
+*
+*Return: void
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+{
+_putchar(i);
+}
+putchar('\n');
+}
