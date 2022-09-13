c329cd2d02d0b0c5e006174a9f43086ab58d5b96
https://github.com/Ijeoma-ukwe/alx-low_level_programming.git <ukwejennifer@gmail.com>
2022-09-13 16:26:58 -0700

    fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-* print _alphabet-function that prints the alphabet in lowercase
+* print_alphabet - functions that prints the alphabet in lowercase
 *
-* Return: Always 0
+* Return: Always 0 (Success)
 */
-
 void print_alphabet(void)
 {
 int i;
