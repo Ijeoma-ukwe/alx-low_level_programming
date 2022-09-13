7f93fa8a508dcfce69f4839665d5b00984de8915
Ijeoma-ukwe <111155128+Ijeoma-ukwe@users.noreply.github.com>
2022-09-14 00:00:35 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,11 +8,5 @@ int _islower(int);
 int _isalpha(int);
 int print_sign(int);
 int _abs(int);
-int print_last_digit(int;
-void jack_bauer(void);
-void times_table(void)
-int add(int, int);
-void print_to_98(int n);
-void print_times_tables(int n);
 
-#endif
+
