8f3460f0f8702df65800af258fe840d000b4d0c0
Ijeoma-ukwe <111155128+Ijeoma-ukwe@users.noreply.github.com>
2022-09-13 22:52:57 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,10 @@
 /**
 * print-alphabet - function that prints the alphabet in lowercase
 *
-*Return: void
+*Return: Always 0
 */
 
-void print_alpphabet(void)
+int print_alpphabet(void)
 {
 int i;
 
@@ -15,4 +15,5 @@ for (i = 'a' ; i <= 'z' ; i++)
 _putchar(i);
 }
 _putchar('\n');
+return (0);
 }
